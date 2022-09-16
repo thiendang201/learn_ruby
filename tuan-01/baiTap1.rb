e00701d6e138bd893402d844d3ebe963207744a2
thiendang201 <thiendang201@gmail.com>
2022-09-16 10:55:09 +0700

    Bai tap tuan 2

--- tuan-01/baiTap1.rb
+++ tuan-01/baiTap1.rb
@@ -6,4 +6,4 @@ puts "Số vừa nhập là: #{a}\n"
 
 print "Nhập vào 2 số: "
 b,c = gets.split
-puts "Giá trị lớn nhất trong 2 số: #{b > c ? b : c}"
+puts "Giá trị lớn nhất trong 2 số: #{b.to_i > c.to_i ? b : c}"
---
+++ tuan-02/bai_1.rb
@@ -0,0 +1,8 @@
+# bai 1
+arr = [4,3,1,5,9]
+
+print "min: ", arr.min, "\n"
+print "max: ", arr.max, "\n"
+print "avg: ", arr.reduce(:+).to_f / arr.length , "\n"
+
+
